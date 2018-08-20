b1b293f7b1c2dfce334b1248e92a337d8225127e
Tim Lee <leewtim@gmail.com>
2018-08-20 00:04:32 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import Projects from './Components/Projects';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      projects: []
+    }
+  }
+  
+  componentWillMount(){
+    this.setState({projects: [
+      {
+        title: 'Business website',
+        category: 'Web Design'
+      },
+      {
+        title: 'Social App',
+        category: 'Mobile Development'
+      },
+      {
+        title: 'Ecommerce Shopping Cart',
+        category: 'Web Development'
+      },
+    ]});
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+       My App
+       <Projects projects={this.state.projects}/>
+      </div>
+    );
+  }
+}
+
+export default App;
