317451019cdae1ef98de740e94c733302ce65e3d
Tim Lee <leewtim@gmail.com>
2018-08-21 23:45:03 -0700

    Added JSON Placeholder API data

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ class App extends Component {
       {
         id: uuid.v4(),
         title: 'Ecommerce Shopping Cart',
-        category: 'Web Development'
+        category: 'Photography'
       },
     ]});
   }
